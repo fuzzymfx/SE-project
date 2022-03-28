fe17a1fabb9f290c35f62663fb0b51976c18058a
Ausaf-Pro <ausafhussain010@gmail.com>
2022-03-28 23:37:36 +0530

    added nav-utils

---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "../components/navbar/index";
+import "./index.css";
+
+function App() {
+  return (
+    <div className="main-canvas">
+      <Navbar />
+      <h1>Hello, user@gmail.com</h1>
+    </div>
+  );
+}
+
+export default App;
